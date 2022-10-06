d439b743467d2560697a57539e2e6249e3fd42e4
Abdusamad98 <abdukarimovabdusamad1998@gmail.com>
2022-10-06 15:07:49 +0500

    add: new changes

--- lib/cubits/publishers/publishers_cubit.dart
+++ lib/cubits/publishers/publishers_cubit.dart
@@ -1,6 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:default_project/data/models/books/book_model.dart';
+import 'package:default_project/data/models/helper/helper_model.dart';
 import 'package:default_project/data/repositories/books_repository.dart';
 import 'package:default_project/data/repositories/publishers_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
 import 'package:meta/meta.dart';
 
 part 'publishers_state.dart';
@@ -9,8 +13,47 @@ class PublishersCubit extends Cubit<PublishersState> {
   PublishersCubit({
     required this.booksRepository,
     required this.publisherRepository,
-  }) : super(PublishersInitial());
+  }) : super(PublishersState(
+          errorText: "",
+          status: FormzStatus.pure,
+          publishers: [],
+          publisherBooks: [],
+        ));
 
   final BooksRepository booksRepository;
   final PublisherRepository publisherRepository;
+
+  void getAllPublishers() async {
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      List<HelperModel> publishers = await []; //get
+      emit(
+        state.copyWith(
+          status: FormzStatus.submissionSuccess,
+          publishers: publishers,
+        ),
+      );
+    } catch (error) {
+      emit(
+        state.copyWith(
+            status: FormzStatus.submissionFailure, errorText: "Error"),
+      );
+    }
+  }
+
+  void getPublisherBooks(int publisherId) async {
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      List<BookModel> books = await []; //get
+      emit(
+        state.copyWith(
+          status: FormzStatus.submissionSuccess,
+          publisherBooks: books,
+        ),
+      );
+    } catch (error) {
+      emit(state.copyWith(
+          status: FormzStatus.submissionFailure, errorText: "Error"));
+    }
+  }
 }
--- lib/cubits/publishers/publishers_state.dart
+++ lib/cubits/publishers/publishers_state.dart
@@ -1,6 +1,35 @@
 part of 'publishers_cubit.dart';
 
-@immutable
-abstract class PublishersState {}
+class PublishersState extends Equatable {
+  PublishersState({
+    required this.errorText,
+    required this.status,
+    required this.publishers,
+    required this.publisherBooks,
+  });
 
-class PublishersInitial extends PublishersState {}
+  final FormzStatus status;
+  final String errorText;
+  final List<HelperModel> publishers;
+  final List<BookModel> publisherBooks;
+
+  PublishersState copyWith({
+    FormzStatus? status,
+    List<HelperModel>? publishers,
+    String? errorText,
+    List<BookModel>? publisherBooks,
+})=>
+      PublishersState(
+          errorText: errorText ?? this.errorText,
+          status: status ?? this.status,
+          publishers: publishers ?? this.publishers,
+          publisherBooks: publisherBooks ?? this.publisherBooks);
+
+  @override
+  List<Object?> get props => [
+        errorText,
+        status,
+        publishers,
+        publisherBooks,
+      ];
+}
